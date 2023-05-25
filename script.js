cae49e41b40237de354d3cf51cdb5486caf715c9
Harsha T R <63777708+TRHarsha@users.noreply.github.com>
2023-05-25 19:48:43 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-toggle").addEventListener("click", function() {
+  var contactList = document.getElementById("contact-list");
+  contactList.classList.toggle("hidden");
+});

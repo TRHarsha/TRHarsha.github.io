783bce60bc0f4b706ee216293143eafee1562dc5
Harsha T R <63777708+TRHarsha@users.noreply.github.com>
2023-07-13 20:01:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-// Get the projects toggle button and the contact list
-var projectsToggle = document.getElementById("projects-toggle");
-var contactList = document.getElementById("contact-list");
-
-// Add an event listener to the projects toggle button
-projectsToggle.addEventListener("click", function() {
-  // Hide the contact list if it's visible
-  contactList.classList.add("hidden");
-  
-  // Open the project URL in a new tab
-  window.open("https://github.com/TRHarsha/TRHarsha.github.io/blob/main/project/proj1/index.html", "_blank");
+document.getElementById("contact-toggle").addEventListener("click", function() {
+  var contactList = document.getElementById("contact-list");
+  contactList.classList.toggle("hidden");
 });
+document.getElementById("projects-toggle").addEventListner("click",function(){
+  var projectList = document.getElementById("project-list");
+  projectList.projectList.toggle("hidden");
+};
